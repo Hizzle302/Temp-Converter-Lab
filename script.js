3bfbee1daa276e2cf181d361746f33663d4d9fa7
Hizzle302 <azcrawley@yahoo.com>
2021-01-11 18:44:32 -0500

    html done

---
+++ script.js
@@ -0,0 +1,8 @@
+// let value = user-Input; 
+// let i = value;
+
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML=(valNum-32)/1.8
+
+}
